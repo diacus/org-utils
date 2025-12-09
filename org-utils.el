c2a1308064858f67a0a6cfd37058afa1ce89cf04
Diacus Magnuz @ Windows <diacus.magnuz@gmail.com>
2025-12-08 20:15:42 -0600

    fix: org-get-entry -> org-entry-get

--- org-utils.el
+++ org-utils.el
@@ -14,7 +14,7 @@
 (defun org-utils/copy-property-value (property-name)
   "Copies the value of PROPERTY-NAME property of the current Org item."
   (interactive "MPROPERTY: ")
-  (let ((value (org-get-entry nil property-name)))
+  (let ((value (org-entry-get nil property-name)))
     (if value
 	(progn
 	  (kill-new value)
@@ -25,7 +25,7 @@
 (defun org-utils/browse-property-url (property-name)
   "Reads the value of PROPERTY-NAME property of the current Org item, if it is a valid URL opens it using the default web browser."
   (interactive "MPROPERTY: ")
-  (let ((url (org-get-entry nil property-name)))
+  (let ((url (org-entry-get nil property-name)))
     (if url
 	(browse-url url)
       (message "Property '%s' not found" property-name))))
